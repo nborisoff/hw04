643199708b78d55e308ebea7821d7c468030f920
nborisoff <nborissov256@gmail.com>
2024-04-18 16:28:32 +0300

    #4 feat: деление по репозиториям

--- src/modules/blogs/controllers/createBlogPostController.ts
+++ src/modules/blogs/controllers/createBlogPostController.ts
@@ -3,22 +3,11 @@ import { blogService } from "../service/blogService";
 import { postMongoQueryRepository } from "../../posts/repositories/postMongoQueryRepository";
 import { RequestParamsBody } from "../../../types/common-types";
 import { BlogIdModel } from "../models/blogs";
-import { blogMongoQueryRepository } from "../repositories/blogMongoQueryRepository";
-import { ObjectId } from "mongodb";
-import { HTTP_STATUSES } from "../../../app/settings";
 
 export const createBlogPost = async (
-  req: RequestParamsBody<BlogIdModel, any>,
-  res: Response,
+    req: RequestParamsBody<BlogIdModel, any>,
+    res: Response,
 ) => {
-  const blog = await blogMongoQueryRepository.find(
-    new ObjectId(req.params.blogId),
-  );
-
-  if (!blog) {
-    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-  }
-
   const createdPost = await blogService.createBlogPost({
     ...req.body,
     blogId: req.params.blogId,
