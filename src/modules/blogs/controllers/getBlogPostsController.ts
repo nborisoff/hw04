99b88eb68faf9d1b5288b112cfef4878408409c3
nborisoff <nborissov256@gmail.com>
2024-04-18 16:13:55 +0300

    #4 feat: деление по репозиториям

--- src/modules/blogs/controllers/getBlogPostsController.ts
+++ src/modules/blogs/controllers/getBlogPostsController.ts
@@ -5,11 +5,21 @@ import { TBlogQueryModel } from "../@types/blogs";
 import { BlogIdModel } from "../models/blogs";
 import { HTTP_STATUSES } from "../../../app/settings";
 import { postMongoQueryRepository } from "../../posts/repositories/postMongoQueryRepository";
+import { blogMongoQueryRepository } from "../repositories/blogMongoQueryRepository";
+import { ObjectId } from "mongodb";
 
 export const getBlogPosts = async (
   req: RequestParamsQuery<BlogIdModel, TBlogQueryModel>,
   res: Response,
 ) => {
+  const blog = await blogMongoQueryRepository.find(
+    new ObjectId(req.params.blogId),
+  );
+
+  if (!blog) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+  }
+
   const sanitizedQuery = helper(req.query);
   const posts = await postMongoQueryRepository.getMany(
     sanitizedQuery,
