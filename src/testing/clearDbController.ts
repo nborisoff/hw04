eddb4de05be60124abf8fbc3a5647cbc5ed68674
nborisoff <nborissov256@gmail.com>
2024-05-12 00:47:59 +0300

    #5 feat: +user model

--- src/testing/clearDbController.ts
+++ src/testing/clearDbController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
-import { blogCollection, postCollection } from "../db/mongo-db";
+import { blogCollection, postCollection, userCollection } from "../db/mongo-db";
 import { HTTP_STATUSES } from "../app/settings";
 
 export const clearDb = async (req: Request, res: Response) => {
   await blogCollection.deleteMany({});
   await postCollection.deleteMany({});
+  await userCollection.deleteMany({});
   res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 };
